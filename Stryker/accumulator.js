d0e2483d394b47b6a99a8951bda4c50d1c94387d
alexander.volkov@honeywell.com <Honeywell10+>
2019-02-27 16:13:30 +0300

    initial commit

---
+++ Stryker/accumulator.js
@@ -0,0 +1,12 @@
+class Accumulator {
+
+    constructor() {
+        this.value = 0;
+    }
+    add(inValue) {
+        this.value += inValue;
+    }
+    sub(inValue) {
+        this.value -= inValue;
+    }
+}
